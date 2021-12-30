c22c908e28b79084988200b6796a97012c1b05f9
rnwathon <rakhmatnashrul@gmail.com>
2021-12-30 20:25:54 +0700

    Add list goal

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,31 @@ import ScoreCard from './components/ScoreCard'
 function App() {
   // STATE
   const [scorePlayer1, setScorePlayer1] = useState(0)
-  const [scorePlayer2, setScorePlayer2] = useState(2)
+  const [scorePlayer2, setScorePlayer2] = useState(0)
+  const [alerts, setAlerts] = useState([])
 
   const ubahScorePlayer1 = () => {
     const newScorePlayer1 = scorePlayer1 + 1
     setScorePlayer1(newScorePlayer1)
+
+    // == ADD NEW ITEM TO ALERT
+    // == CONTOH 1
+    // const newAlerts = alerts.concat(["Goal by Player 1"])
+    // setAlerts(newAlerts)
+    // == CONTOH 2
+    // const arr = [...alerts]
+    // arr.push("Goal by Player 1")
+    // setAlerts(arr)
+    // == CONTOH 3
+    setAlerts([...alerts, "Goal by Player 1"])
   }
 
   const ubahScorePlayer2 = () => {
     const newScorePlayer2 = scorePlayer2 + 1
     setScorePlayer2(newScorePlayer2)
+
+    // == ADD NEW ITEM TO ALERT
+    setAlerts([...alerts, "Goal by Player 2"])
   }
 
   return (
@@ -34,6 +49,16 @@ function App() {
           handleChangeScoreP1={ubahScorePlayer1}
           handleChangeScoreP2={ubahScorePlayer2}
         />
+
+        {
+          alerts.map((item, idx) => {
+            return(
+              <div key={"alert-" + idx} className="alert alert-primary">
+                {item}
+              </div>
+            )
+          })
+        }
       </section>
     </div>
   );
